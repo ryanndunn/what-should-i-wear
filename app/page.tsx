77d87ffd9e64f7797af1be139e72de8745c95f56
ryanatduckpin <ryan.d@rollwithduckpin.com>
2024-04-08 14:38:49 -0400

    added geocoding for zips to coords, added open weather api back in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import { ApiKeys } from "@/app/secrets/api-keys";
 
-async function fetchWeather() {
+async function fetchZipCode() {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/zip?zip=21234,US&appid=${ApiKeys['open_weather_map']}`
+  );
+
+  if (response.ok) {
+    const responseBody = await response.json();
+    console.log(responseBody);
+    return responseBody;
+  }
+}
+
+async function fetchGovWeather(lat: number,lon: number) {
 
   const pointsResponse = await fetch(
-    `https://api.weather.gov/points/39.401859,-76.605049`
+    `https://api.weather.gov/points/${lat},${lon}`
   );
 
   if (pointsResponse.ok) {
@@ -27,17 +39,35 @@ async function fetchWeather() {
   }
 }
 
-export default async function Home() {
-  const data = await fetchWeather();
+async function fetchOpenWeather(lat: number,lon: number) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${ApiKeys['open_weather_map']}`
+  );
+  if (response.ok) {
+    const responseBody = await response.json();
+    return responseBody;
+  }
+}
 
-  console.log(data.properties);
-  console.log(data.properties.periods[0].probabilityOfPrecipitation);
-  console.log(data.properties.periods[0].dewpoint);
-  console.log(data.properties.periods[0].relativeHumidity);
+export default async function Home() {
+  const fetchZip = await fetchZipCode();
+  const lat = fetchZip.lat;
+  const lon = fetchZip.lon;
+  const govData = await fetchGovWeather(lat,lon);
+  const openData = await fetchOpenWeather(lat,lon);
+
+  console.log(govData.properties);
+  console.log(openData);
+  // console.log(govData.properties.periods[0].probabilityOfPrecipitation);
+  // console.log(govData.properties.periods[0].dewpoint);
+  // console.log(govData.properties.periods[0].relativeHumidity);
+
+  //console.log(openData);
 
   return (
     <main className="font-poppins mx-10">
       <p>What Should I Wear Today?</p>
+      
     </main>
   );
 }
